c0d4156de5a5e6d6aeb82db23e120026be502887
tokuhausu <tokuhausu@gmail.com>
2020-06-26 00:53:58 +0900

    [add] Extensions:KindExt for BlockObject.Kind

---
+++ TetrisCore/Source/Extension/ObjectKindExt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using static TetrisCore.Source.BlockObject;
+
+namespace TetrisCore.Source.Extension
+{
+    public static class KindExt
+    {
+        /// <summary>
+        /// ブロックの色を取得
+        /// </summary>
+        /// <param name="self">オブジェクトの種類</param>
+        /// <returns>色</returns>
+        public static Color BlockColor(this Kind self)
+        {
+            switch (self)
+            {
+                case Kind.I: return Color.Aqua;
+                case Kind.O: return Color.Yellow;
+                case Kind.T: return Color.Purple;
+                case Kind.J: return Color.Blue;
+                case Kind.L: return Color.Orange;
+                case Kind.S: return Color.Green;
+                case Kind.Z: return Color.Red;
+
+            }
+            throw new InvalidOperationException("Unknown Kind");
+        }
+    }
+}
