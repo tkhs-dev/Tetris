4004a1b2e743add77fd0fae2b47ba4297f60df8f
tokuhausu <38522336+tokuhausu@users.noreply.github.com>
2020-06-25 18:01:34 +0900

    [add] Block.Kind and static BlockObject is added

--- TetrisCore/Source/BlockObject.cs
+++ TetrisCore/Source/BlockObject.cs
@@ -17,10 +17,68 @@ namespace TetrisCore.Source
         {
             get { return _color; }
         }
+
+        /// <param name="data">ObjectData 0:empty 1:block 2:block_point 3:empty_point</param>
+        /// <param name="color">Object Color</param>
         public BlockObject(int[,] data, Color color)
         {
             _data = data;
             _color = color;
         }
+
+        public static class Kind
+        {
+            public static BlockObject I =
+                new BlockObject(
+                    new int[,]{
+                        {0,0,0,0},
+                        {1,2,1,1},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                    },Color.Aqua);
+            public static BlockObject T =
+                new BlockObject(
+                    new int[,]{
+                        {0,1,0},
+                        {1,1,1},
+                        {0,0,0}
+                    }, Color.Purple);
+            public static BlockObject S =
+                new BlockObject(
+                    new int[,]{
+                        {0,1,1},
+                        {1,1,0},
+                        {0,0,0}
+                    }, Color.Green);
+            public static BlockObject Z =
+                new BlockObject(
+                    new int[,]{
+                        {1,1,0},
+                        {0,1,1},
+                        {0,0,0}
+                    }, Color.Red);
+            public static BlockObject L =
+                new BlockObject(
+                    new int[,]{
+                        {0,0,1},
+                        {1,1,1},
+                        {0,0,0}
+                    }, Color.Orange);
+        }
+        public static BlockObject J =
+                new BlockObject(
+                    new int[,]{
+                        {1,0,0},
+                        {1,1,1},
+                        {0,0,0}
+                    }, Color.Blue);
+        public static BlockObject O =
+                new BlockObject(
+                    new int[,]{
+                        {1,1},
+                        {1,1}
+                    }, Color.Orange);
+
+
     }
 }
