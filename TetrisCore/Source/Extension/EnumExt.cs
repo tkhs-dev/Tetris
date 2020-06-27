5fd8a2de8bd7f4873cf6fb907efce98746a5952e
tokuhausu <tokuhausu@gmail.com>
2020-06-27 12:13:45 +0900

    [update] rename from KindExt.And add Directions.

---
+++ TetrisCore/Source/Extension/EnumExt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using static TetrisCore.Source.BlockObject;
+
+namespace TetrisCore.Source.Extension
+{
+    public static class DirectionsExt
+    {
+        public static Directions Rotate(this Directions self,int num)
+        {
+            return (Directions)Enum.ToObject(typeof(Directions),(((int)self)+num) % 4);            
+        }
+    }
+}
