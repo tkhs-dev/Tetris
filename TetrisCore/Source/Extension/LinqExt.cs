c1280897c1593fee6a1fa5d81f91cf5338792f28
tokuhausu <tokuhausu@gmail.com>
2020-06-27 17:00:44 +0900

    [add] Linq.Chunk

---
+++ TetrisCore/Source/Extension/LinqExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TetrisCore.Source.Extension
+{
+    public static class LinqExt
+    {
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> self,int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException("Chunk size must be greater than 0.", nameof(size));
+
+            return self.Select((v, i) => new { v, i })
+                .GroupBy(x => x.i / size)
+                .Select(g => g.Select(x => x.v));
+        }
+    }
+}
