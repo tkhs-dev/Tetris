2b260c45249b8655cb3a6f05675e87fc0b7af384
tokuhausu <tokuhausu@gmail.com>
2020-06-27 15:54:32 +0900

    [add] GetObject

--- TetrisCore/Source/Extension/EnumExt.cs
+++ TetrisCore/Source/Extension/EnumExt.cs
@@ -7,11 +7,101 @@ using static TetrisCore.Source.BlockObject;
 
 namespace TetrisCore.Source.Extension
 {
+    /// <see cref="BlockObject.Directions"/>
     public static class DirectionsExt
     {
         public static Directions Rotate(this Directions self,int num)
         {
             return (Directions)Enum.ToObject(typeof(Directions),num>=0 ? (((int)self)+num) % 4 : ((((int)self) + num) % 4)+4);            
+        }
+    }
+    /// <see cref="BlockObject.Kind"/>
+    public static class KindExt
+    {
+        private static readonly BlockObject Object_I =
+            new BlockObject(
+                    Color.Aqua,
+                    new int[,]{
+                        {0,0,0,0},
+                        {1,2,1,1},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                    });
+        private static readonly BlockObject Object_O =
+            new BlockObject(
+                    Color.Yellow,
+                    new int[,]{
+                        {0,0,0,0},
+                        {1,2,1,1},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                    });
+        private static readonly BlockObject Object_T =
+            new BlockObject(
+                    Color.Purple,
+                    new int[,]{
+                        {0,0,0,0},
+                        {1,2,1,1},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                    });
+        private static readonly BlockObject Object_J =
+            new BlockObject(
+                    Color.Blue,
+                    new int[,]{
+                        {0,0,0,0},
+                        {1,2,1,1},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                    });
+        private static readonly BlockObject Object_L =
+            new BlockObject(
+                    Color.Orange,
+                    new int[,]{
+                        {0,0,0,0},
+                        {1,2,1,1},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                    });
+        private static readonly BlockObject Object_S =
+            new BlockObject(
+                    Color.Green,
+                    new int[,]{
+                        {0,0,0,0},
+                        {1,2,1,1},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                    });
+        private static readonly BlockObject Object_Z =
+            new BlockObject(
+                    Color.Red,
+                    new int[,]{
+                        {0,0,0,0},
+                        {1,2,1,1},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                    });
+
+        public static BlockObject GetObject(this Kind self)
+        {
+            switch (self)
+            {
+                case Kind.I:
+                    return Object_I;
+                case Kind.O:
+                    return Object_O;
+                case Kind.T:
+                    return Object_T;
+                case Kind.J:
+                    return Object_J;
+                case Kind.L:
+                    return Object_L;
+                case Kind.S:
+                    return Object_S;
+                case Kind.Z:
+                    return Object_Z;
+            }
+            throw new InvalidOperationException("Unknown Kind");
         }
     }
 }
