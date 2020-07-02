346f1c6ef9ccf5b137bd8ce3e11ad4eb4e965a10
tokuhausu <tokuhausu@gmail.com>
2020-07-03 00:04:30 +0900

    [update]

--- TetrisPlayer/Program.cs
+++ TetrisPlayer/Program.cs
@@ -17,7 +17,8 @@ namespace TetrisPlayer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Form maini = new Form1();
+            Application.Run(maini);
 
             GetLogger().Info("Start up TetrisPlayer...");
         }
