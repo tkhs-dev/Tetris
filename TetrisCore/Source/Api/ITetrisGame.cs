03c962123e6dcde44058a6d562389cf1b91fbeb1
tokuhausu <tokuhausu@gmail.com>
2020-07-24 13:07:52 +0900

    [update] initializeの引数を追加

--- TetrisCore/Source/Api/ITetrisGame.cs
+++ TetrisCore/Source/Api/ITetrisGame.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,6 @@ namespace TetrisCore.Source.Api
 {
     public interface ITetrisGame
     {
-        void initialize();
+        void initialize(TetrisGame game);
     }
 }
