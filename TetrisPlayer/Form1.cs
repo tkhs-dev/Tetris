325d44d984a909917d7183938ab071da547f51cf
tokuhausu <tokuhausu@gmail.com>
2020-07-24 13:10:01 +0900

    [add] 描画対象のコントロールを探してRendererに登録する

--- TetrisPlayer/Form1.cs
+++ TetrisPlayer/Form1.cs
@@ -7,17 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TetrisCore.Source;
+using TetrisCore.Source.Api;
 
 namespace TetrisPlayer
 {
     public partial class Form1 : Form
     {
+        TetrisGame Game = new TetrisGame(TetrisPlayer.GetLogger());
         public Form1()
         {
             InitializeComponent();
+            this.Shown += initialized;
 
             // 最大化を無効にする
-            this.MaximizeBox = false;
+            //this.MaximizeBox = false;
+        }
+        private void initialized(object sender,EventArgs a)
+        {
+            foreach(Control c in this.Controls)
+            {
+                if(c is IRenderer){
+                    TetrisPlayer.GetLogger().Info("Find a Renderer:"+c.GetType().Name);
+                    Game.SetRenderer((IRenderer)c);
+                }
+            }
         }
     }
 }
