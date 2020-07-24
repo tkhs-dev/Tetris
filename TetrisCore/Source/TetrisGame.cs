2412ccddedc69bba9456309a6783b775e46f93a0
tokuhausu <tokuhausu@gmail.com>
2020-07-24 13:10:29 +0900

    [update] 機能を追加

--- TetrisCore/Source/TetrisGame.cs
+++ TetrisCore/Source/TetrisGame.cs
@@ -1,11 +1,44 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TetrisCore.Source.Api;
 
 namespace TetrisCore.Source
 {
     public class TetrisGame
     {
-        
+        internal ILog logger;
+
+        private Field field;
+
+        public readonly int ROW;
+        public readonly int COLUMN;
+
+        private IRenderer renderer;
+        private IController controller;
+
+        public TetrisGame(ILog logger,int row = 10,int column = 20)
+        {
+            this.logger = logger;
+            logger.Info("TetrisInstance Creating");
+            ROW = row;
+            COLUMN = column;
+            field = new Field(row, column);
+            field.OnBlockChanged += Draw;
+        }
+        public void SetRenderer(IRenderer renderer)
+        {
+            this.renderer = renderer;
+            renderer.initialize(this);
+        }
+        public void SetController(IController controller)
+        {
+            this.controller = controller;
+        }
+        private void Draw(Object sender,Field field)
+        {
+            renderer.Render(field);
+        }
     }
 }
