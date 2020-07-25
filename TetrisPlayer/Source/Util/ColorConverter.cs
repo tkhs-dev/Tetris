3f41ec49472f99494d23cf11aecec2985107c26c
tokuhausu <tokuhausu@gmail.com>
2020-07-25 13:44:37 +0900

    [add] ColorConverterを追加

---
+++ TetrisPlayer/Source/Util/ColorConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisPlayer.Source.Util
+{
+    public class ColorConverter
+    {
+        public static SharpDX.Color GetDXColor(System.Drawing.Color color)
+        {
+            return new SharpDX.Color(color.ToArgb());
+        }
+    }
+}
