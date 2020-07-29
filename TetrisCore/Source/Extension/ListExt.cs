dca3609efe016c47fea3f9943c6d9cff5db0eb23
tokuhausu <tokuhausu@gmail.com>
2020-07-29 14:07:35 +0900

    [add] Listの拡張関数を追加

---
+++ TetrisCore/Source/Extension/ListExt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetrisCore.Source.Extension
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// 先頭にあるオブジェクトを削除し、返します
+        /// </summary>
+        public static T Dequeue<T>(this IList<T> self)
+        {
+            var result = self[0];
+            self.RemoveAt(0);
+            return result;
+        }
+
+        /// <summary>
+        /// 末尾にオブジェクトを追加します
+        /// </summary>
+        public static void Enqueue<T>(this IList<T> self, T item)
+        {
+            self.Add(item);
+        }
+
+        /// <summary>
+        /// 先頭にあるオブジェクトを削除せずに返します
+        /// </summary>
+        public static T Peek<T>(this IList<T> self)
+        {
+            return self[0];
+        }
+        ///<summary>
+        ///リストからランダムに一つ取り出す
+        /// </summary>
+        public static T GetRandom<T>( this IList<T> list )
+	    {
+            Random random = new Random();
+		    return list[random.Next(0, list.Count)];
+	    }
+}
+}
