1452b0525191805bccf1d1c8e2d12e57c4f2b65c
tokuhausu <tokuhausu@gmail.com>
2020-09-06 22:05:12 +0900

    [update] 回転処理を改善

--- TetrisCore/Source/Extension/EnumExt.cs
+++ TetrisCore/Source/Extension/EnumExt.cs
@@ -12,7 +12,8 @@ namespace TetrisCore.Source.Extension
     {
         public static Directions Rotate(this Directions self,int num)
         {
-            return (Directions)Enum.ToObject(typeof(Directions),num>=0 ? (((int)self)+num) % 4 : ((((int)self) + num+444444) % 4));            
+            Console.WriteLine((int)self + num >= 0 ? (((int)self) + num) % 4 : ((int)self + num) % 4 + 4);
+            return (Directions)Enum.ToObject(typeof(Directions), (int)self + num >= 0 ? (((int)self) + num) % 4 : ((int)self+num)%4+4) ;            
         }
     }
     /// <see cref="BlockObject.Kind"/>
