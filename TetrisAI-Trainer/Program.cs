c7f99e01cceefdc2ba885ce3afa920c677d22f77
tokuhausu <tokuhausu@gmail.com>
2020-09-14 23:29:17 +0900

    [add] TetrisAI-Trainerを追加,log4netを設定

---
+++ TetrisAI-Trainer/Program.cs
@@ -0,0 +1,14 @@
+﻿using log4net;
+using System;
+
+namespace TetrisAI_Trainer
+{
+    class Program
+    {
+        private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
+        }
+    }
+}
