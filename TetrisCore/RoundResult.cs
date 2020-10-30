19100334a0d89b2c0f724b67f3254c392225b093
tokuhausu <tokuhausu@gmail.com>
2020-10-31 00:53:17 +0900

    [update] UUIDを削除

--- TetrisCore/RoundResult.cs
+++ TetrisCore/RoundResult.cs
@@ -8,17 +8,16 @@ namespace TetrisCore
     {
         public RoundResult(Field start, Field end, BlockObject obj, int[] removedLines, int erodedObjectCells)
         {
-            ID = Guid.NewGuid();
             this.FieldAtStart = start;
             this.FieldAtEnd = end;
             this.Object = obj;
             this.RemovedLines = removedLines;
             this.ErodedObjectCells = erodedObjectCells;
         }
-        public Guid ID { get; }
         public Field FieldAtStart { get; }
         public Field FieldAtEnd { get; }
         public BlockObject Object { get; }
+        public BlockPosition Position => Object.Position;
         public int[] RemovedLines { get; }
         public int ErodedObjectCells { get; }
     }
