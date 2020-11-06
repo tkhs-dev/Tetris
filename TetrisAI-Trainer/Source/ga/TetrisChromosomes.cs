cf542014cc7a8656d76e9140cebe4bf76b2b1e77
tokuhausu <tokuhausu@gmail.com>
2020-11-06 12:14:55 +0900

    [add] GeneticSharpを導入

---
+++ TetrisAI-Trainer/Source/ga/TetrisChromosomes.cs
@@ -0,0 +1,16 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetrisAI_Trainer.Source.ga
+{
+    public class TetrisChromosomes
+    {
+        FloatingPointChromosome chromosome;
+        public TetrisChromosomes()
+        {
+            chromosome = new FloatingPointChromosome(new double[] {-10,-10,-10 },new double[] {10,10,10 },new int[] {32,32,32 },new int[] { });
+        }
+    }
+}
---
+++ TetrisAI-Trainer/Source/ga/TetrisFitness.cs
@@ -0,0 +1,16 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Fitnesses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetrisAI_Trainer.Source.ga
+{
+    class TetrisFitness : IFitness
+    {
+        public double Evaluate(IChromosome chromosome)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
