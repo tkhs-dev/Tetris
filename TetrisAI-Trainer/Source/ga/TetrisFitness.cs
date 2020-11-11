2277428b18600c1f643da31eeb8303eca63e79a3
tokuhausu <38522336+tokuhausu@users.noreply.github.com>
2020-11-11 17:00:44 +0900

    [update] 並列処理を改善

--- TetrisAI-Trainer/Source/ga/TetrisFitness.cs
+++ TetrisAI-Trainer/Source/ga/TetrisFitness.cs
@@ -15,16 +15,21 @@ namespace TetrisAI_Trainer.Source.ga
     {
         public double Evaluate(IChromosome chromosome)
         {
-            const int sample = 2;
+            const int sample = 3;
             EvaluationNNParameter parameter = (chromosome as TetrisChromosome).GetParameter();
             Evaluator evaluator = new Evaluator(parameter);
             List<TetrisGame> games = Enumerable.Range(0, sample).Select(x => new TetrisGame(TetrisAITrainer.Logger)).ToList();
-            games.ForEach(x=> { x.SetController(new AITetrisController(evaluator)); });
-            games.ForEach(x => { x.MaxRound = 200; });
-            games.ForEach(x => { x.Start(); });
-            Task<GameResult>[] tasks = games.Select(x => x.WhenGameEnd()).ToArray();
-            Task.WaitAll(tasks);
-            GameResult[] results = tasks.Select(x => x.Result).ToArray();
+            List<Task<GameResult>> tasks = new List<Task<GameResult>>(); 
+            Parallel.ForEach(games, async x => 
+            {
+                x.SetController(new AITetrisController(evaluator));
+                x.MaxRound = 200;
+                x.Start();
+                tasks.Add(x.WhenGameEnd());
+            });
+            Task<GameResult[]> task = Task.WhenAll(tasks);
+            task.Wait();
+            GameResult[] results = task.Result;
             double av = results.Average(x => x.Score+x.Round*x.Round*100);
             TetrisAITrainer.Logger.Info(av);
             return av;
