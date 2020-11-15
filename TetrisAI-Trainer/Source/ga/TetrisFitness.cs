1db5f7098e22be174272591d8272333b25072976
tokuhausu <tokuhausu@gmail.com>
2020-11-15 23:21:58 +0900

    [add] fitnessの並列演算に中断処理を追加

--- TetrisAI-Trainer/Source/ga/TetrisFitness.cs
+++ TetrisAI-Trainer/Source/ga/TetrisFitness.cs
@@ -1,7 +1,9 @@
 ﻿using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Fitnesses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using TetrisAI.Source;
 using TetrisCore.Source;
@@ -30,15 +32,35 @@ namespace TetrisAI_Trainer.Source.ga
             Evaluator evaluator = new Evaluator(parameter);
             List<TetrisGame> games = Enumerable.Range(0, Sample).Select(x => new TetrisGame(TetrisAITrainer.Logger)).ToList();
             List<GameResult> results = new List<GameResult>();
-            /*
-            Parallel.ForEach(games, x =>
+
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            ParallelOptions options = new ParallelOptions();
+            options.CancellationToken = tokenSource.Token;
+            try
             {
-                x.SetController(new AITetrisController(evaluator,0));
-                x.MaxRound = 200;
-                x.Start();
-                results.Add(x.WhenGameEnd().Result);
-            });*/
-            Parallel.ForEach(games, x =>
+                Parallel.ForEach(games, options, x =>
+                {
+                    x.SetController(new AITetrisController(evaluator, 0));
+                    x.TimerEnabled = true;
+                    x.TimerSpan = 100;
+                    x.MaxRound = MaxRound;
+                    x.Start();
+                    var result = x.WhenGameEnd().Result;
+                    if (result.Score == 0)
+                    {
+                        //tokenSource.Cancel();
+                    }
+                    results.Add(result);
+                    x.Dispose();
+                });
+            }
+            catch (OperationCanceledException e)
+            {
+                TetrisAITrainer.Logger.Info(0);
+                return 0;
+            }
+            /*
+             games.ForEach(x =>
             {
                 x.SetController(new AITetrisController(evaluator, 0));
                 x.TimerEnabled = true;
@@ -48,8 +70,8 @@ namespace TetrisAI_Trainer.Source.ga
                 results.Add(x.WhenGameEnd().Result);
                 x.Dispose();
             });
-            if (results.Any(x => x.Score == 0)) return 0;
-            double av = results.Average(x => x.Score);
+             */
+            double av = results.Average(x => x.Score+x.Round*x.Round);
             TetrisAITrainer.Logger.Info(av);
             return av;
         }
